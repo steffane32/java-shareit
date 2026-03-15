72ef1a19aadf4c405e0bac191159f110be43487c
Olga Zavalishina <steffane@yandex.ru>
2026-03-15 21:45:54 +0300

    sprint 15 homework v1.3
    поправила тесты и настройки для тестов..забыла изначально про них вообще

--- src/test/java/ru/practicum/shareit/SimpleTest.java
+++ src/test/java/ru/practicum/shareit/SimpleTest.java
@@ -2,8 +2,10 @@ package ru.practicum.shareit;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class SimpleTest {
     @Test
     public void testContext() {
